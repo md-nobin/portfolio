9a6b0f3e795e34facaa4b8b6c56d434788b063a4
mdnobin96670 <farhanahmednobin@gmail.com>
2024-02-14 20:01:50 +0600

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'MD Nobin',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Nobin is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    siteUrl: 'https://md-nobin.netlify.app/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@md-nobin',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'MD Nobin',
+        short_name: 'MD Nobin',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const IconLogo = () => (
+const IconLoader = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
     <title>Logo</title>
     <g transform="translate(-8.000000, -2.000000)">
@@ -21,4 +21,5 @@ const IconLogo = () => (
   </svg>
 );
 
-export default IconLogo;
+export default IconLoader;
+// export default IconLogo;
